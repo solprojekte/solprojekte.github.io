872eae31c7e5029b2b7f6da988c8b51262f3f9b5
heliacer <heliacer35@gmail.com>
2024-06-07 20:32:54 +0200

    nav links

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
       <Hero />
       <img src={geto} className='geto' alt="" />
       <div className='content'>
-        <h3>Unsere Vision</h3>
+        <h3 id='vision'>Unsere Vision</h3>
         <p>Wir möchten die Anerkennung der Geschlechtergleichstellung auf lokaler und globaler Ebene fördern. Unsere Mission ist es, das Bewusstsein für die bestehenden Ungleichheiten zu schärfen und zur Lösung beizutragen. Mit dieser Webseite wollen wir einen zentralen Überblick und fundierte Informationen zum Thema Geschlechtergleichstellung bieten.</p>
         <button>press me</button>
       </div>
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react';
 import Logo from '/logo.svg';
-import { animateScroll as scroll } from 'react-scroll';
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 export default function Nav() {
   const [isOnTop, setIsOnTop] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsOnTop(window.scrollY <= 500);
+      setIsOnTop(window.scrollY <= 400);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -16,12 +16,36 @@ export default function Nav() {
     };
   }, []);
 
+  const [isActive, setIsActive] = useState(false);
+
+  const handleHeaderClick = () => {
+    scroll.scrollToTop();
+  };
+
+  const handleMouseDown = () => {
+    setIsActive(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsActive(false);
+  };
+
   return (
     <nav className={isOnTop ? '' : 'scrolled'}>
-      <div className='site-header' onClick={() => scroll.scrollToTop()}>
-        <img className='logo-image' src={Logo} alt="Logo" />
+      <div className='site-header' onClick={handleHeaderClick} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
+        <img className={`logo-image ${isActive ? 'active' : ''}`} src={Logo} alt="Logo" />
         <h2>GETO</h2>
       </div>
+      <div className='vr'></div>
+      <Link to='vision' smooth spy offset={-100}><h3>Vision</h3></Link>
+      <div className='sep'/>
+      <Link to='' smooth spy offset={-100}><h3>Umfrage</h3></Link>
+      <div className='sep'/>
+      <Link to='' smooth spy offset={-100}><h3>Weltweit</h3></Link>
+      <div className='sep'/>
+      <Link to='' smooth spy offset={-100}><h3>Politik & Recht</h3></Link>
+      <div className='sep'/>
+      <Link to='' smooth spy offset={-100}><h3>Schweiz</h3></Link>
     </nav>
   );
 }
